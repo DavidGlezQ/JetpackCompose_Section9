f143a8488ec0054b29a171d43c0b57daf7c489c6
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-02-12 21:38:59 -0600

    section9 jc
    Classe 77 toolbar

--- app/src/main/java/com/davidgllez/jetpackcompose_section9/MainActivity.kt
+++ app/src/main/java/com/davidgllez/jetpackcompose_section9/MainActivity.kt
@@ -1,43 +1,59 @@
 package com.davidgllez.jetpackcompose_section9
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.davidgllez.jetpackcompose_section9.ui.theme.JetpackCompose_Section9Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose_Section9Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
+                    topBar = { ToolbarForm () },
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                    backgroundColor = MaterialTheme.colors.background,
+                    content = {
+                        CFrom()
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JetpackCompose_Section9Theme {
-        Greeting("Android")
+        CFrom()
     }
+}
+
+@Composable
+fun CFrom() {
+
+}
+
+@Composable
+fun ToolbarForm() { //Toolbar
+    TopAppBar(title = { Text(text = stringResource(id = R.string.app_name))}, //Titulo de toolbar
+    actions = {// Menu de toolbar
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(painter = painterResource(id = R.drawable.ic_send), contentDescription = stringResource(
+                            R.string.app_bar_action_send))
+        }
+    })
 }
