a0ebb1b9f893fb704019326462a169f9fbc06edf
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-02-12 21:47:01 -0600

    section 9 jc
    Classe 78, colors

--- app/src/main/java/com/davidgllez/jetpackcompose_section9/ui/theme/Color.kt
+++ app/src/main/java/com/davidgllez/jetpackcompose_section9/ui/theme/Color.kt
@@ -1,8 +1,9 @@
 package com.davidgllez.jetpackcompose_section9.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.davidgllez.jetpackcompose_section9.R
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Blue200 = Color(0xFF90CAF9)
+val Blue500 = Color(0xFF2196F3)
+val Blue700 = Color(0xFF1976D2)
 val Teal200 = Color(0xFF03DAC5)
